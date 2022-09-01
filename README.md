410c72eb244832578088fad253b385c68f73c75a
olgaminaieva <minaievao@gmail.com>
2022-09-01 20:39:32 +0200

    add docker files + readme file

---
+++ README.md
@@ -0,0 +1,21 @@
+## Campaign Team WebdriverIO (WDIO) Automation set-up
+This project demonstrate automation test suites set-up for Campaign team.
+Automation testing will be done with WebdriverIO help, using page object pattern and Docker images set-up.
+## System Requirements
+- Node.js v16.15.1 or greater (https://nodejs.org/)
+- npm v8.11.0 or greater (https://www.npmjs.com/)
+- java 
+- docker
+
+## How to start
+1. npm install
+2. npm run docker:start
+3. npm run test
+4. npm run report
+
+## Docker notes
+1. Install Docker on the local machine: https://www.docker.com/get-started/
+2. run the command: docker-compose up
+3. 
+
+https://www.npmjs.com/package/webdriver-manager
--- wdio.conf.js
+++ wdio.conf.js
@@ -111,11 +111,12 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver', 'geckodriver', 'docker', [
+    services: [//'chromedriver', 'geckodriver',
+        'docker',
+        [
         "image-comparison",
         // The options
         {
-            // Some options, see the docs for more
             baselineFolder: join(process.cwd(), "./tests/sauceLabsBaseline/"),
             formatImageName: "{tag}-{logName}-{width}x{height}",
             screenshotPath: join(process.cwd(), ".tmp/"),
@@ -139,7 +140,8 @@ exports.config = {
             },
             // ... more options
         },
-    ],],
+    ]
+    ],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
